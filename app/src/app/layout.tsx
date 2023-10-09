38e90300d9dc4877f602f8c8c3ff46a8ac54120f
k5.trismegistus <k5.trismegistus@gmail.com>
2023-10-09 09:25:26 +0900

    :lipstick: Add global header

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -3,6 +3,10 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import Link from "next/link";
+import App from "next/app";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,6 +22,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              K-NEXT
+            </Typography>{" "}
+            <Link href="/comics">
+              <Button color="inherit">Read comics</Button>
+            </Link>
+            <Link href="/comics/new">
+              <Button color="inherit">Upload new comic</Button>
+            </Link>
+            <Link href="/videos">
+              <Button color="inherit">Watch videos</Button>
+            </Link>
+            <Link href="/videos/new">
+              <Button color="inherit">Upload new video</Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
         <TrpcProvider>{children}</TrpcProvider>
       </body>
     </html>
